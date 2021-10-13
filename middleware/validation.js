d053bb8282ffb6919a032f12bf1aa98a7a7e22ad
zenklot <zenklot11@gmail.com>
2021-10-13 18:47:07 +0700

    Menambah Validasi Baru

--- middleware/validation.js
+++ middleware/validation.js
@@ -9,6 +9,14 @@ const validUserReg = (data) => {
   return schema.validate(data);
 };
 
+const validForgetPwd = (data) => {
+  const schema = Joi.object({
+    password: Joi.string().min(6).required(),
+  });
+  return schema.validate(data);
+};
+
 module.exports = {
   validUserReg,
+  validForgetPwd,
 };
