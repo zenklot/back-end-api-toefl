9b6bf87955a691d99fd4398071686e95b1564bcf
zenklot <zenklot11@gmail.com>
2021-10-13 18:48:07 +0700

    Memperbaiki endpoint dan penamaan fungsi

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const { authLogin, authEmailVerify } = require('../middleware/verifyToken');
+const {
+  authLogin, authEmailVerify, refreshTokenVerify, forgetPwdToken,
+} = require('../middleware/verifyToken');
 
 const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 
 router.get('/', authLogin, AuthController.methodGet);
-router.post('/auth/register', AuthController.register);
-router.post('/auth/login', AuthController.login);
-router.post('/auth/refresh-token', AuthController.postRefreshToken);
-router.get('/auth/logout', authLogin, AuthController.logout);
-router.put('/auth/verify-email/:token', authEmailVerify, AuthController.emailVerifycation);
+router.post('/register', AuthController.postRegister);
+router.post('/login', AuthController.postLogin);
+router.post('/refresh-token', refreshTokenVerify, AuthController.postRefreshToken);
+router.get('/logout', authLogin, AuthController.getLogout);
+router.put('/verify-email/:token', authEmailVerify, AuthController.putEmailVerifycation);
+router.get('/:id/verify-email', authLogin, AuthController.getEmailValidation);
+router.post('/forget-password/', AuthController.postForgetPassword);
+router.put('/forget-password/', forgetPwdToken, AuthController.putForgetPassword);
+
 module.exports = router;
